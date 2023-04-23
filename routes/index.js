35365ec4db9cddf4b9b5b326f09248f38150820f
MasterP2 <arikpopeace98@gmail.com>
2023-04-23 00:05:52 -0400

    initial commit

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const routes = require("express").Router();
-
-
-const myController = require("../controllers");
-
-routes.get("/", myController.myNameFunction);
-routes.get("/another", myController.anotherNameFunction);
-
-module.exports = routes;
