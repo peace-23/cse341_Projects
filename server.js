47e26ae67ff7112efe9a2a87ec1c4b9a817fafd9
MasterP2 <arikpopeace98@gmail.com>
2023-04-28 22:54:53 -0400

    moved the dev dependency

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+var bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
 const app = express();
