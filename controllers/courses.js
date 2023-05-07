9480f0ea3c5478c3434249c9a7399520e358a19f
MasterP2 <arikpopeace98@gmail.com>
2023-05-06 21:14:24 -0400

    Added error handling

--- controllers/courses.js
+++ controllers/courses.js
@@ -5,7 +5,10 @@ const ObjectId = require('mongodb').ObjectId;
 
 const allCourses = async (req, res) => {
     const result = await mongodb.getDb().db().collection('courses').find();
-    result.toArray().then((lists) => {
+    result.toArray((err, lists) => {
+        if (err) {
+            res.status(400).json({ message: err });
+        }
         res.setHeader('Content-Type', 'application/json');
         res.status(200).json(lists);
     });
@@ -14,9 +17,12 @@ const allCourses = async (req, res) => {
 const singleCourse = async (req, res) => {
     const userId = new ObjectId(req.params.id);
     const result = await mongodb.getDb().db().collection('courses').find({ _id: userId });
-    result.toArray().then((lists) => {
+    result.toArray((err, result) => {
+        if (err) {
+            res.status(400).json({ message: err });
+        }
         res.setHeader('Content-Type', 'application/json');
-        res.status(200).json(lists[0]);
+        res.status(200).json(result[0]);
     });
 };
 
--- readme.md
+++ readme.md
@@ -9,3 +9,5 @@ This API is intended for a Project in CSE 341 to perform CRUD operations on a Mo
 - npm start
 
 
+## How to use this repository
+Each branch contains a collection of code for a given assignment or class activity. Select a branch, and then either clone, fork, or download the zip file to access the files on your computer.
