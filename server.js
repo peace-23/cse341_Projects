ea3240ae04f7ffbd6117a01e650fbd43f76b0464
MasterP2 <arikpopeace98@gmail.com>
2023-05-09 22:10:44 -0400

    recreated Swagger.json

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
 
 
+
 const port = process.env.PORT || 8080;
 const app = express();
 
 app
+    // .use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
     .use(bodyParser.json())
     .use((req, res, next) => {
         res.setHeader('Access-Control-Allow-Origin', '*');
